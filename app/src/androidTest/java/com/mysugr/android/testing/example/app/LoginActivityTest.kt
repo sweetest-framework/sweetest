ac6d2dc4c26173ccf32c4f7c3b6056bc40ff362b
Marc Neureiter <mneureiter@juvavis.com>
2018-09-12 14:31:40 +0200

    Refine UI test and add interaction test

--- app/src/androidTest/java/com/mysugr/android/testing/example/app/LoginActivityTest.kt
+++ app/src/androidTest/java/com/mysugr/android/testing/example/app/LoginActivityTest.kt
@@ -1,8 +1,9 @@
 package com.mysugr.android.testing.example.app
 
 
+import android.support.test.InstrumentationRegistry
 import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.action.ViewActions.*
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.rule.ActivityTestRule
@@ -12,8 +13,10 @@ import com.mysugr.android.testing.example.app.view.LoginViewModel.State
 import com.mysugr.android.testing.example.app.view.LoginViewModel.State.*
 import com.mysugr.android.testing.example.app.view.StateListener
 import org.junit.After
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.mockito.Mockito.*
 
 
 class LoginActivityTest {
@@ -21,52 +24,91 @@ class LoginActivityTest {
     @get:Rule
     var activityTestRule = ActivityTestRule(LoginActivity::class.java, true, false)
 
+    private lateinit var loginViewModel: ILoginViewModel
+    private lateinit var stateListener: StateListener
+
+    private var state: State = LoggedOut()
+        set(value) {
+            field = value
+            stateListener(value)
+        }
+
+    @Before
+    fun before() {
+
+        loginViewModel = mock(ILoginViewModel::class.java)
+
+        DependencyFramework.loginViewModel = loginViewModel
+
+        stateListener = let { _ ->
+            var waitForListener: StateListener? = null
+            `when`(loginViewModel.run { stateListener = any<StateListener>() ?: {} }).then {
+                @Suppress("UNCHECKED_CAST")
+                waitForListener = it.arguments[0] as StateListener
+                Any()
+            }
+            activityTestRule.launchActivity(null)
+            InstrumentationRegistry.getInstrumentation().waitForIdleSync()
+            waitForListener ?: error("Listener not set by Activity")
+        }
+
+    }
+
     @After
     fun after() {
         DependencyFramework.reset()
     }
 
-    class LoginViewModelMock : ILoginViewModel {
-        override var stateListener: StateListener = {}
-        override var state: State = LoggedOut()
-        override fun loginOrRegister(email: String, password: String) {}
-        override fun logout() {}
+    @Test
+    fun testStateChange() {
+
+        state = LoggedOut()
+        onView(withId(R.id.login_progress))
+                .check(matches(withEffectiveVisibility(Visibility.GONE)))
+        onView(withId(R.id.login_form))
+                .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+
+        state = Busy()
+        onView(withId(R.id.login_progress))
+                .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+        onView(withId(R.id.login_form))
+                .check(matches(withEffectiveVisibility(Visibility.GONE)))
+
+        state = Error()
+        onView(withId(R.id.login_progress))
+                .check(matches(withEffectiveVisibility(Visibility.GONE)))
+        onView(withId(R.id.login_form))
+                .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+
+        state = LoggedIn(true)
+        onView(withId(R.id.logout_button))
+                .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+
     }
 
+
     @Test
-    fun stateChangeTest() {
+    fun testInteraction() {
 
-        val mock = LoginViewModelMock()
-
-        fun setState(state: State) {
-            mock.state = state
-            mock.stateListener(state)
+        run {
+            val expectedEmail = "email@test.com"
+            val expectedPassword = "supersecure"
+            state = LoggedOut()
+            onView(withId(R.id.email))
+                    .perform(replaceText(expectedEmail))
+            onView(withId(R.id.password))
+                    .perform(replaceText(expectedPassword))
+            onView(withId(R.id.sign_in_button))
+                    .perform(click())
+            verify(loginViewModel).loginOrRegister(expectedEmail, expectedPassword)
         }
 
-        DependencyFramework.loginViewModel = mock
-
-        activityTestRule.launchActivity(null)
-
-        setState(LoggedOut())
-        onView(withId(R.id.login_progress))
-                .check(matches(withEffectiveVisibility(Visibility.GONE)))
-        onView(withId(R.id.login_form))
-                .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
-
-        setState(Busy())
-        onView(withId(R.id.login_progress))
-                .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
-        onView(withId(R.id.login_form))
-                .check(matches(withEffectiveVisibility(Visibility.GONE)))
-
-        setState(Error())
-        onView(withId(R.id.login_progress))
-                .check(matches(withEffectiveVisibility(Visibility.GONE)))
-        onView(withId(R.id.login_form))
-                .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
-
-        setState(LoggedIn(true))
-        onView(withId(R.id.logout_button)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+        run {
+            state = LoggedIn(true)
+            onView(withId(R.id.logout_button))
+                    .perform(click())
+            verify(loginViewModel).logout()
+        }
 
     }
 
