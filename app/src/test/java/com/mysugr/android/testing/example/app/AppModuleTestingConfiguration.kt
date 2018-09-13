4b81296be03436724c848117daf8dcf020cf4971
Marc Neureiter <mneureiter@juvavis.com>
2018-09-13 14:46:04 +0200

    Fix tests

--- app/src/test/java/com/mysugr/android/testing/example/app/AppModuleTestingConfiguration.kt
+++ app/src/test/java/com/mysugr/android/testing/example/app/AppModuleTestingConfiguration.kt
@@ -5,12 +5,12 @@ import com.mysugr.android.testing.example.app.view.LoginViewModel
 import com.mysugr.android.testing.example.auth.AuthManager
 import com.mysugr.android.testing.example.net.BackendGateway
 import com.mysugr.android.testing.example.net.DummyBackendGateway
-import com.mysugr.android.testing.example.state.SessionStore
+import com.mysugr.android.testing.example.state.DummySessionStore
 import com.mysugr.testing.framework.configuration.moduleTestingConfiguration
 
 val appModuleTestingConfiguration = moduleTestingConfiguration {
 
-    dependency any initializer { SessionStore() }
+    dependency any initializer { DummySessionStore() }
     dependency any initializer { DummyBackendGateway() as BackendGateway }
     dependency any initializer { AuthManager(instanceOf(), instanceOf()) }
     dependency any initializer { LoginViewModel(instanceOf()) }
