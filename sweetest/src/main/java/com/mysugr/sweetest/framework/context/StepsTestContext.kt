96b29a0ae572ee3506cf27558de5599d44edbf26
Marc Neureiter <mneureiter@juvavis.com>
2018-09-15 18:00:49 +0200

    Improved error messages when accessing steps

--- sweetest/src/main/java/com/mysugr/sweetest/framework/context/StepsTestContext.kt
+++ sweetest/src/main/java/com/mysugr/sweetest/framework/context/StepsTestContext.kt
@@ -15,17 +15,21 @@ class StepsTestContext(private val testContext: TestContext) {
     private val map = mutableMapOf<Class<*>, Steps>()
     private var setUpDone = false
 
-    private fun checkSetUp() {
-        if (!setUpDone) {
-            throw IllegalStateException("finalizeSetUp() not yet called")
+    private fun checkSetUp(clazz: KClass<*>) {
+        check(setUpDone) {
+            "You are trying to access steps class \"$clazz\" before the all initialization steps had finished." +
+                    "Probably you are retrieving the steps class outside an appropriate setup code block which " +
+                    "should be registered in a steps or test class with `override fun configure() = " +
+                    "super.configure().onSetUp { ... }`."
         }
     }
 
-    private fun checkNotYetSetUp() {
-        if (setUpDone) {
-            throw IllegalStateException("Call not expected after finalization. If you run under Cucumber make sure " +
-                "the steps class is initialized on time. You can force initialization by adding a " +
-                "@Before-annotated function to the class.")
+    private fun checkNotYetSetUp(clazz: KClass<*>) {
+        check(!setUpDone) {
+            "You are trying to access steps class \"$clazz\" which has not " +
+                    "yet been initialized. That can happen when you are using a BDD framework and it's not " +
+                    "initializing the steps class before testing starts. You can fix that by adding a dummy function " +
+                    "in the steps class with a @Before annotation (this forces the class to be instantiated earlier)."
         }
     }
 
@@ -46,7 +50,8 @@ class StepsTestContext(private val testContext: TestContext) {
 
     @PublishedApi
     internal fun <T : Steps> get(clazz: KClass<T>): T {
-        checkSetUp()
+        checkSetUp(clazz)
+        @Suppress("UNCHECKED_CAST")
         return map[clazz.java] as? T ?: create(clazz.java)
     }
 
@@ -60,7 +65,7 @@ class StepsTestContext(private val testContext: TestContext) {
         return try {
             if (!required.contains<Class<*>>(clazz)) {
                 throw RuntimeException("Steps class \"$clazz\" has not yet been marked as required! Each steps class " +
-                    "has to be set up as required before it's used!")
+                        "has to be set up as required before it's used!")
             }
             checkType(kClass)
             checkConstructor(kClass)
@@ -69,8 +74,8 @@ class StepsTestContext(private val testContext: TestContext) {
             newInstance
         } catch (exception: Exception) {
             throw RuntimeException("Could not automatically create steps class \"$clazz\". Either fix the underlying " +
-                "cause (see nested exception), instantiate it manually so it will be added to the list of " +
-                "steps classes or make sure Cucumber instantiated the steps class!", exception)
+                    "cause (see nested exception), instantiate it manually so it will be added to the list of " +
+                    "steps classes or make sure Cucumber instantiated the steps class!", exception)
         }
     }
 
@@ -88,31 +93,31 @@ class StepsTestContext(private val testContext: TestContext) {
             }
             val constructor = constructors.first()
             if (constructor.parameters.size > 1 ||
-                constructor.parameters.first().type != TestContext::class.starProjectedType) {
+                    constructor.parameters.first().type != TestContext::class.starProjectedType) {
                 throw RuntimeException("Wrong constructor")
             }
         } catch (exception: Exception) {
             throw RuntimeException("\"$clazz\", as all steps classes which you want to auto-instanciate, should " +
-                "have exactly one constructor receiving a TestContext object!", exception)
+                    "have exactly one constructor receiving a TestContext object!", exception)
         }
     }
 
     @PublishedApi
     internal fun setUpInstance(instance: Steps) {
-        checkNotYetSetUp()
+        checkNotYetSetUp(instance::class)
         checkType(instance::class)
         val clazz = instance::class.java
         if (map.containsKey(clazz)) {
             throw RuntimeException("An instance of steps class \"${instance.javaClass}\" has already been " +
-                "registered! If you run under Cucumber make sure the steps class is initialized on time. " +
-                "You can force initialization by adding a @Before-annotated function to the class.")
+                    "registered! If you run under Cucumber make sure the steps class is initialized on time. " +
+                    "You can force initialization by adding a @Before-annotated function to the class.")
         }
         map[clazz] = instance
     }
 
     @PublishedApi
     internal fun setUpAsRequired(kClass: KClass<Steps>) {
-        checkNotYetSetUp()
+        checkNotYetSetUp(kClass)
         val clazz = kClass.java
         if (!required.contains(clazz)) {
             required.add(clazz)
@@ -120,6 +125,6 @@ class StepsTestContext(private val testContext: TestContext) {
     }
 
     class GetStepsClassException(clazz: KClass<*>) : RuntimeException("Could not get steps class of type \"$clazz\": " +
-        "it has not yet been registered. Are you sure you have created an instance of the steps class before? Is " +
-        "it created by Cucumber? Have you used the correct base class \"BaseNewSteps\" for the steps class?")
+            "it has not yet been registered. Are you sure you have created an instance of the steps class before? Is " +
+            "it created by Cucumber? Have you used the correct base class \"BaseNewSteps\" for the steps class?")
 }
