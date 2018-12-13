f96c1b996a09e1a70a9eef2cf966802c8ea6389d
Daniel Gehrer <xa17d@users.noreply.github.com>
2018-12-13 09:19:15 +0100

    Add yieldForEachJob extension

--- sweetest/src/main/java/com/mysugr/sweetest/framework/coroutine/TestExtensions.kt
+++ sweetest/src/main/java/com/mysugr/sweetest/framework/coroutine/TestExtensions.kt
@@ -2,10 +2,7 @@ package com.mysugr.sweetest.framework.coroutine
 
 import com.mysugr.sweetest.framework.base.BaseJUnitTest
 import com.mysugr.sweetest.framework.base.Steps
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 
 fun BaseJUnitTest.testCoroutine(
         testBlock: suspend CoroutineScope.() -> Unit
@@ -26,3 +23,14 @@ suspend fun Deferred<*>.throwExceptionIfFailed() {
         await() // throws exception, if Deferred failed. Does nothing otherwise
     }
 }
+
+/**
+ * With multiple child jobs, you may want to yield multiple times to ensure each child can finish.
+ * This function counts the number of child jobs and calls [yield] the number of times as jobs are present.
+ */
+suspend fun CoroutineScope.yieldForEachJob() {
+    val job = coroutineContext[Job.Key] ?: kotlin.error("coroutineContext doesn't have a parent Job.")
+    kotlin.repeat(countJobs(job)) { yield() }
+}
+
+private fun countJobs(job: Job): Int = 1 + job.children.sumBy { countJobs(it) }
