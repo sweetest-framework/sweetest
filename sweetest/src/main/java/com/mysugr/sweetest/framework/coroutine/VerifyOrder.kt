f3d2142ae2d085232354ac389898088605dcb579
Daniel Gehrer <xa17d@users.noreply.github.com>
2018-12-13 16:12:45 +0100

    Add KDoc for verifyOrder

--- sweetest/src/main/java/com/mysugr/sweetest/framework/coroutine/VerifyOrder.kt
+++ sweetest/src/main/java/com/mysugr/sweetest/framework/coroutine/VerifyOrder.kt
@@ -1,9 +1,39 @@
 package com.mysugr.sweetest.framework.coroutine
 
+/**
+ * Verifies a specific call order. If the actual order is different from the expected one, an
+ * [AssertionError] is thrown.
+ * Successful example:
+ * ```
+ * verifyOrder {
+ *   order(1) // is called first
+ *
+ *   launch {
+ *     order(3) // this is called after yield()
+ *   }
+ *
+ *   order(2) // this is called second
+ *   yield()
+ *   order(4) // this is called after the launch block
+ * }
+ * ```
+ *
+ * Failing example:
+ * ```
+ * verifyOrder {
+ *   order(1)
+ *   order(3) // this will throw an [AssertionError], because order(2) was expected here instead
+ *   order(2)
+ * }
+ * ```
+ */
 suspend fun verifyOrder(block: suspend OrderVerifier.() -> Unit) {
     block(OrderVerifier())
 }
 
+/**
+ * Helper class for [verifyOrder].
+ */
 class OrderVerifier {
     private var currentOrderIndex = 0
 
@@ -11,7 +41,9 @@ class OrderVerifier {
         currentOrderIndex++
 
         if (orderIndex != currentOrderIndex) {
-            throw AssertionError("Expected order($currentOrderIndex), but reached order($orderIndex) instead")
+            throw AssertionError(
+                    "Expected order($currentOrderIndex), but reached order($orderIndex) instead"
+            )
         }
     }
 }
