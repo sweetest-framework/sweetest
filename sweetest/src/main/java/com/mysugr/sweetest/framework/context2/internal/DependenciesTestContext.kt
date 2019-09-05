0ad8c8af6c18d0eda0b5b52e78509bc04ed844b7
Marc Neureiter <marc.neureiter@mysugr.com>
2019-09-05 17:34:38 +0200

    Dependencies test context accessors

--- sweetest/src/main/java/com/mysugr/sweetest/framework/context2/internal/DependenciesTestContext.kt
+++ sweetest/src/main/java/com/mysugr/sweetest/framework/context2/internal/DependenciesTestContext.kt
@@ -1,8 +1,37 @@
 package com.mysugr.sweetest.framework.context2.internal
 
 import com.mysugr.sweetest.framework.dependency2.DependencyState
+import com.mysugr.sweetest.framework.dependency2.getOrInitializeInstance
+import java.lang.IllegalStateException
 import kotlin.reflect.KClass
+import kotlin.reflect.full.isSuperclassOf
 
 class DependenciesTestContext {
     private val dependencyStates = mutableMapOf<KClass<*>, DependencyState>()
+
+    fun <T : Any> getDependencyStateFor(type: KClass<T>): DependencyState {
+        return dependencyStates[type] ?: throw IllegalStateException(
+            "Dependency state not initialized, please use standard dependency() function to access dependencies!"
+        )
+    }
+
+    fun assignDependencyStateFor(assignedType: KClass<*>, dependencyState: DependencyState) {
+        checkTypeCompatibility(assignedType, dependencyState.type)
+        if (dependencyStates.containsKey(assignedType)) {
+            throw IllegalArgumentException("State for dependency type \"${assignedType.simpleName}\" is already present.")
+        }
+        dependencyStates[assignedType] = dependencyState
+    }
+}
+
+fun <T : Any> DependenciesTestContext.consumeDependency(type: KClass<T>): T =
+    getDependencyStateFor(type).getOrInitializeInstance(type)
+
+private fun checkTypeCompatibility(assignedType: KClass<*>, dependencyType: KClass<*>) {
+    if (assignedType != dependencyType && !assignedType.isSuperclassOf(dependencyType)) {
+        throw IllegalArgumentException(
+            "You are trying to assign a dependency of type \"${dependencyType.simpleName}\" to " +
+                "\"${assignedType.simpleName}\", which is not possible."
+        )
+    }
 }
---
+++ sweetest/src/test/java/com/mysugr/sweetest/framework/context2/internal/dependencies/RetrievalTest.kt
@@ -0,0 +1,33 @@
+package com.mysugr.sweetest.framework.context2.internal.dependencies
+
+import com.mysugr.sweetest.framework.context2.internal.DependenciesTestContext
+import com.mysugr.sweetest.framework.dependency2.DependencyState
+import org.junit.Test
+
+class RetrievalTest {
+
+    @Test
+    fun `Can retrieve test state`() {
+        val sut = DependenciesTestContext()
+        val state = DependencyState(A::class) { A() }
+        sut.assignDependencyStateFor(A::class, state)
+        val actual = sut.getDependencyStateFor(A::class)
+        assert(actual === state)
+    }
+
+    @Test
+    fun `Can retrieve test state for various assigned types`() {
+        val sut = DependenciesTestContext()
+        val state = DependencyState(AA::class) { AA() }
+        sut.assignDependencyStateFor(AA::class, state)
+        sut.assignDependencyStateFor(A::class, state)
+        val actual1 = sut.getDependencyStateFor(A::class)
+        val actual2 = sut.getDependencyStateFor(AA::class)
+        assert(actual1 === state)
+        assert(actual2 === state)
+    }
+
+    open class A
+
+    open class AA : A()
+}
