d37176ea1604962db17f37d16415981311929136
Marc Neureiter <marc.neureiter@mysugr.com>
2020-02-05 09:11:55 +0100

    Remove unnecessary ability to have a custom CoroutineContext

--- sweetest/src/main/java/com/mysugr/sweetest/framework/coroutine/DefaultCoroutinesTestContext.kt
+++ sweetest/src/main/java/com/mysugr/sweetest/framework/coroutine/DefaultCoroutinesTestContext.kt
@@ -1,37 +1,25 @@
 package com.mysugr.sweetest.framework.coroutine
 
-import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.async
 import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.DelayController
-import kotlinx.coroutines.test.TestCoroutineDispatcher
-import kotlinx.coroutines.test.TestCoroutineExceptionHandler
 import kotlinx.coroutines.test.TestCoroutineScope
-import kotlinx.coroutines.test.UncaughtExceptionCaptor
 import kotlinx.coroutines.test.UncompletedCoroutinesError
 import kotlin.coroutines.ContinuationInterceptor
 import kotlin.coroutines.CoroutineContext
-import kotlin.coroutines.EmptyCoroutineContext
 
-class DefaultCoroutinesTestContext(
-    private val configuration: CoroutinesTestConfigurationData,
-    /**
-     * An optional context that MAY provide [UncaughtExceptionCaptor] and/or [DelayController] for the
-     * TestCoroutineScope
-     */
-    coroutineContext: CoroutineContext = EmptyCoroutineContext
-) : CoroutinesTestContext {
+class DefaultCoroutinesTestContext(private val configuration: CoroutinesTestConfigurationData) : CoroutinesTestContext {
 
-    override val coroutineScope = TestCoroutineScope(coroutineContext.checkArguments())
+    val job = SupervisorJob()
+    override val coroutineScope = TestCoroutineScope(job)
 
     override fun runTest(testBody: suspend () -> Unit) {
-        val parentJob = checkNotNull(coroutineScope.coroutineContext[Job])
         val dispatcher = checkNotNull(coroutineScope.coroutineContext[ContinuationInterceptor] as? DelayController)
 
-        val startingJobs = parentJob.activeJobs()
+        val startingJobs = job.activeJobs()
         val deferred = coroutineScope.async {
             testBody()
         }
@@ -41,32 +29,15 @@ class DefaultCoroutinesTestContext(
         }
         if (configuration.autoCancelTestCoroutinesEnabled) {
             runBlocking {
-                parentJob.cancelAndJoin()
+                job.cancelAndJoin()
             }
         }
         coroutineScope.cleanupTestCoroutines()
-        val endingJobs = parentJob.activeJobs()
+        val endingJobs = job.activeJobs()
         if ((endingJobs - startingJobs).isNotEmpty()) {
             throw UncompletedCoroutinesError("Test finished with active jobs: $endingJobs")
         }
     }
-}
-
-private fun CoroutineContext.checkArguments(): CoroutineContext {
-    val dispatcher = get(ContinuationInterceptor).run {
-        this?.let { require(this is DelayController) { "Dispatcher must implement DelayController: $this" } }
-        this ?: TestCoroutineDispatcher()
-    }
-
-    val exceptionHandler = get(CoroutineExceptionHandler).run {
-        this?.let {
-            require(this is UncaughtExceptionCaptor) { "coroutineExceptionHandler must implement UncaughtExceptionCaptor: $this" }
-        }
-        this ?: TestCoroutineExceptionHandler()
-    }
-
-    val job = get(Job) ?: SupervisorJob()
-    return this + dispatcher + exceptionHandler + job
 }
 
 private fun CoroutineContext.activeJobs(): Set<Job> {
