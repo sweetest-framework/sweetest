a8f00cd37c52cd0ba89b3bf122fa699f28e07850
Marc Neureiter <marc.neureiter@mysugr.com>
2020-02-05 16:07:15 +0100

    Use ordinal instead of custom field in enum

--- sweetest/src/main/java/com/mysugr/sweetest/framework/flow/InitializationStep.kt
+++ sweetest/src/main/java/com/mysugr/sweetest/framework/flow/InitializationStep.kt
@@ -1,18 +1,19 @@
 package com.mysugr.sweetest.framework.flow
 
-enum class InitializationStep(val order: Int) {
-    INITIALIZE_FRAMEWORK(0),
-    INITIALIZE_STEPS(1),
-    INITIALIZE_DEPENDENCIES(2),
-    SET_UP(3),
-    RUNNING(4),
-    TEAR_DOWN(5),
-    DONE(6);
+enum class InitializationStep {
+    UNINITIALIZED,
+    INITIALIZE_FRAMEWORK,
+    INITIALIZE_STEPS,
+    INITIALIZE_DEPENDENCIES,
+    SET_UP,
+    RUNNING,
+    TEAR_DOWN,
+    DONE;
 
-    fun isBeforeOrSame(other: InitializationStep) = other.order >= order
-    fun isBefore(other: InitializationStep) = other.order > order
-    fun isAfter(other: InitializationStep) = other.order < order
-    fun isAfterOrSame(other: InitializationStep) = other.order <= order
+    fun isBeforeOrSame(other: InitializationStep) = other.ordinal >= ordinal
+    fun isBefore(other: InitializationStep) = other.ordinal > ordinal
+    fun isAfter(other: InitializationStep) = other.ordinal < ordinal
+    fun isAfterOrSame(other: InitializationStep) = other.ordinal <= ordinal
 
-    fun getNext() = values()[order + 1]
+    fun getNext() = values()[ordinal + 1]
 }
