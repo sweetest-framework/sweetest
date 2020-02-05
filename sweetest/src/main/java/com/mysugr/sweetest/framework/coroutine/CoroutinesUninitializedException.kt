0f4623ff4c937067413d9eb02a34f0e9c23ff737
Marc Neureiter <marc.neureiter@mysugr.com>
2020-02-05 16:25:47 +0100

    Adapt CoroutinesUninitializedException message
    Ability to do coroutines initialization earlier was added, so message had to be changed.

--- sweetest/src/main/java/com/mysugr/sweetest/framework/coroutine/CoroutinesUninitializedException.kt
+++ sweetest/src/main/java/com/mysugr/sweetest/framework/coroutine/CoroutinesUninitializedException.kt
@@ -1,7 +1,6 @@
 package com.mysugr.sweetest.framework.coroutine
 
 class CoroutinesUninitializedException : Exception(
-    "sweetest's coroutines capabilities are used outside its valid timeframe! Please prevent using them before or " +
-        "after the test function is run. Don't access them in constructors, class or property initializers, but " +
-        "consider using `lazy { ... }` or do initialization in `.onSetUp { ... }` instead."
+    "sweetest's coroutines capabilities are used in a wrong place! " +
+        "Please prevent using them after the test function is run."
 )
