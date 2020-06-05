29275df593b89dfab5ac477812f1a7dc2b1f8d52
Daniel Gehrer <daniel.gehrer@mysugr.com>
2020-06-05 15:44:22 +0200

    Inline test classes to make tests more readable

--- sweetest/src/test/java/com/mysugr/sweetest/ProvideTest.kt
+++ sweetest/src/test/java/com/mysugr/sweetest/ProvideTest.kt
@@ -1,31 +1,30 @@
 package com.mysugr.sweetest
 
 import com.mysugr.sweetest.framework.base.BaseJUnitTest
-import com.mysugr.sweetest.framework.base.BaseSteps
 import com.mysugr.sweetest.framework.base.dependency
-import com.mysugr.sweetest.framework.base.steps
-import com.mysugr.sweetest.framework.configuration.ModuleTestingConfiguration
 import com.mysugr.sweetest.framework.configuration.moduleTestingConfiguration
-import com.mysugr.sweetest.framework.context.TestContext
+import com.mysugr.sweetest.framework.dependency.DependencyConfigurations
 import com.mysugr.sweetest.framework.environment.TestEnvironment
+import com.mysugr.sweetest.util.isMock
 import com.mysugr.sweetest.util.mock
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
-import org.mockito.Mockito.`when`
 
 class ProvideTest {
-
-    companion object {
-        lateinit var moduleTestingConfiguration: ModuleTestingConfiguration
-    }
 
     interface TestUserService {
         fun getUserName(): String
     }
 
+    class FakeTestUserService : TestUserService {
+        override fun getUserName(): String = "FAKE"
+    }
+
     class TestViewModel(
-        private val userService: TestUserService
+        internal val userService: TestUserService
     ) {
         fun reload() {
             message = "Hello ${userService.getUserName()}!"
@@ -35,161 +34,128 @@ class ProvideTest {
             private set
     }
 
-    class TestUserServiceFakeSteps(testContext: TestContext) :
-        BaseSteps(testContext, moduleTestingConfiguration) {
-
-        override fun configure() = super.configure()
-            .provide<TestUserService> { FakeTestUserService() }
-
-        private var username: String = "TEST"
-
-        fun givenUsername(username: String) {
-            this.username = username
-        }
-
-        private inner class FakeTestUserService : TestUserService {
-            override fun getUserName(): String = username
-        }
-    }
-
-    /**
-     * This test class exists only to verify that `provide` can work together with the deprecated
-     * `requireMock`. This test class and the corresponding test can be removed when `requireMock`
-     * is removed.
-     */
-    class TestUserServiceMockSteps(testContext: TestContext) :
-        BaseSteps(testContext, moduleTestingConfiguration) {
-
-        override fun configure() = super.configure()
-            .requireMock<TestUserService>()
-            .onSetUp {
-                `when`(instance.getUserName()).thenAnswer { username }
-            }
-
-        private val instance by dependency<TestUserService>()
-
-        private var username: String = "TEST"
-
-        fun givenUsername(username: String) {
-            this.username = username
-        }
-    }
-
-    class TestViewModelSteps(testContext: TestContext) :
-        BaseSteps(testContext, moduleTestingConfiguration) {
-        override fun configure() = super.configure()
-            .provide<TestViewModel>()
-
-        private val instance by dependency<TestViewModel>()
-
-        fun whenReload() {
-            instance.reload()
-        }
-
-        fun thenMessageIs(expectedMessage: String) {
-            assertEquals(expectedMessage, instance.message)
-        }
-    }
-
-    class TestClassWithFakeSteps : BaseJUnitTest(moduleTestingConfiguration) {
-        private val userService by steps<TestUserServiceFakeSteps>()
-        private val viewModel by steps<TestViewModelSteps>()
-
-        fun exampleTest() {
-            userService.givenUsername("exampleFoo")
-            viewModel.whenReload()
-            viewModel.thenMessageIs("Hello exampleFoo!")
-        }
-    }
-
-    class TestClassWithMockSteps : BaseJUnitTest(moduleTestingConfiguration) {
-        private val userService by steps<TestUserServiceMockSteps>()
-        private val viewModel by steps<TestViewModelSteps>()
-
-        fun exampleTest() {
-            userService.givenUsername("exampleBar")
-            viewModel.whenReload()
-            viewModel.thenMessageIs("Hello exampleBar!")
-        }
-    }
-
-    class TestClassWithDoubleProvide : BaseJUnitTest(moduleTestingConfiguration) {
-        override fun configure() = super.configure()
-            .provide<TestViewModel> { TestViewModel(mock()) }
-            .provide<TestViewModel>()
-
-        val instance by dependency<TestViewModel>()
-    }
-
     @Before
     fun setUp() {
         TestEnvironment.fullReset()
     }
 
     @Test
-    fun `when configured with any, provides can be used`() {
-        givenConfiguredWithAny()
-        // This test class uses `TestUserServiceFakeSteps`, which uses `provide {...}` for
-        // `TestUserService`.
-        // Test service is then used in `TestViewModelSteps` to create a real instance of
-        // `TestViewModel` using `provide<TestViewModel>()`.
-        TestClassWithFakeSteps().run {
+    fun `provide can be used when dependency is configured with any`() {
+        val moduleTestingConfiguration = moduleTestingConfiguration {
+            dependency any of<TestUserService>()
+            dependency any of<TestViewModel>()
+        }
+
+        class TestClass : BaseJUnitTest(moduleTestingConfiguration) {
+            override fun configure() = super.configure()
+                .provide<TestUserService> { FakeTestUserService() }
+                .provide<TestViewModel>()
+
+            val userService by dependency<TestUserService>()
+            val viewModel by dependency<TestViewModel>()
+        }
+
+        TestClass().run {
             junitBefore()
-            exampleTest()
+
+            assertEquals(TestViewModel::class.java, viewModel::class.java)
+            assertEquals(FakeTestUserService::class.java, userService::class.java)
+            assertEquals(userService, viewModel.userService)
+        }
+    }
+
+    /**
+     * Note: This test makes use of a bug: `realOnly` and `mockOnly` are not enforced.
+     */
+    @Test
+    fun `provide can be used when dependency is configured with mockOnly or realOnly`() {
+        val moduleTestingConfiguration = moduleTestingConfiguration {
+            dependency realOnly of<TestUserService>()
+            dependency mockOnly of<TestViewModel>()
+        }
+
+        class TestClass : BaseJUnitTest(moduleTestingConfiguration) {
+            override fun configure() = super.configure()
+                .provide<TestUserService> { FakeTestUserService() }
+                .provide<TestViewModel>()
+
+            val userService by dependency<TestUserService>()
+            val viewModel by dependency<TestViewModel>()
+        }
+
+        TestClass().run {
+            junitBefore()
+
+            assertEquals(TestViewModel::class.java, viewModel::class.java)
+            assertEquals(FakeTestUserService::class.java, userService::class.java)
+            assertEquals(userService, viewModel.userService)
+            assertFalse(viewModel.isMock)
         }
     }
 
     @Test
-    fun `provides works together with mockOnly`() {
-        givenServiceConfiguredAsMockOnly()
-        // This test class uses `TestUserServiceMockSteps`, which uses `requireMock` for
-        // `TestUserService`.
-        // Test service is then used in `TestViewModelSteps` to create a real instance of
-        // `TestViewModel` using `provide<TestViewModel>()`.
-        TestClassWithMockSteps().run {
+    fun `provide works together with mockOnly and requireMock`() { // TODO MARKER
+        val moduleTestingConfiguration = moduleTestingConfiguration {
+            dependency mockOnly of<TestUserService>()
+            dependency any of<TestViewModel>()
+        }
+
+        class TestClass : BaseJUnitTest(moduleTestingConfiguration) {
+            override fun configure() = super.configure()
+                .requireMock<TestUserService>()
+                .provide<TestViewModel>()
+
+            val userService by dependency<TestUserService>()
+            val viewModel by dependency<TestViewModel>()
+        }
+
+        TestClass().run {
             junitBefore()
-            exampleTest()
+
+            assertEquals(TestViewModel::class.java, viewModel::class.java)
+            assertTrue(viewModel.userService.isMock)
         }
     }
 
-    @Test(expected = RuntimeException::class)
-    fun `no dependencies configured leads to exception`() {
-        givenNothingConfigured()
-        TestClassWithFakeSteps().run {
+    @Test(expected = DependencyConfigurations.NotFoundException::class)
+    fun `exception if dependency is not declared in module configuration`() {
+        val moduleTestingConfiguration = moduleTestingConfiguration { }
+
+        class TestClass : BaseJUnitTest(moduleTestingConfiguration) {
+            override fun configure() = super.configure()
+                .provide<TestUserService> { FakeTestUserService() }
+
+            val userService by dependency<TestUserService>()
+        }
+
+        TestClass().run {
             junitBefore()
-            exampleTest()
+            userService // access dependency
         }
     }
 
     @Test(expected = RuntimeException::class)
     fun `provide cannot be used twice for the same type`() {
-        givenConfiguredWithAny()
-        TestClassWithDoubleProvide().run {
+        val moduleTestingConfiguration = moduleTestingConfiguration {
+            dependency any of<TestUserService>()
+            dependency any of<TestViewModel>()
+        }
+
+        class TestClass : BaseJUnitTest(moduleTestingConfiguration) {
+            override fun configure() = super.configure()
+                .provide<TestViewModel> { TestViewModel(mock()) }
+                .provide<TestViewModel>()
+
+            val userService by dependency<TestUserService>()
+            val viewModel by dependency<TestViewModel>()
+        }
+
+        TestClass().run {
             junitBefore()
-        }
-    }
 
-    private fun givenNothingConfigured() {
-        moduleTestingConfiguration = moduleTestingConfiguration { }
-    }
-
-    private fun givenConfiguredWithAny() {
-        moduleTestingConfiguration = moduleTestingConfiguration {
-            dependency mockOnly initializer {
-                object : TestUserService {
-                    override fun getUserName(): String {
-                        return ""
-                    }
-                }
-            }
-            dependency realOnly of<TestViewModel>()
-        }
-    }
-
-    private fun givenServiceConfiguredAsMockOnly() {
-        moduleTestingConfiguration = moduleTestingConfiguration {
-            dependency mockOnly of<TestUserService>()
-            dependency mockOnly of<TestViewModel>()
+            assertEquals(TestViewModel::class.java, viewModel::class.java)
+            assertEquals(FakeTestUserService::class.java, userService::class.java)
+            assertEquals(userService, viewModel.userService)
         }
     }
 }
