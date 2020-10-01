874e9469a6c8762ed4dc37d3809fed58549c7c4d
Marc Neureiter <marc.neureiter@mysugr.com>
2020-10-01 07:43:24 +0200

    Legacy polymorphism test

---
+++ sweetest/src/test/java/com/mysugr/sweetest/LegacyPolymorphismTest.kt
@@ -0,0 +1,77 @@
+package com.mysugr.sweetest
+
+import com.mysugr.sweetest.framework.base.BaseJUnitTest
+import com.mysugr.sweetest.framework.base.dependency
+import com.mysugr.sweetest.framework.configuration.moduleTestingConfiguration
+import com.mysugr.sweetest.framework.environment.TestEnvironment
+import org.junit.After
+import org.junit.Test
+
+class LegacyPolymorphismTest {
+
+    interface Animal
+
+    class Cat : Animal
+
+    @After
+    fun tearDown() {
+        TestEnvironment.fullReset()
+    }
+
+    @Test
+    fun `Consuming supertype gives subtype`() {
+
+        val config = moduleTestingConfiguration {
+            dependency realOnly of<Cat>()
+        }
+
+        val test = object : BaseJUnitTest(config) {
+            val animal by dependency<Animal>()
+        }
+
+        assert(test.animal::class == Cat::class)
+    }
+
+    @Test(expected = Exception::class)
+    fun `Consuming subtype doesn't give supertype`() {
+
+        val config = moduleTestingConfiguration {
+            dependency realOnly of<Animal>()
+        }
+
+        object : BaseJUnitTest(config) {
+            val cat by dependency<Cat>() // fails already here
+        }
+    }
+
+    @Test
+    fun `Locally configuring supertype configures subtype`() {
+
+        val config = moduleTestingConfiguration {
+            dependency any of<Cat>()
+        }
+
+        val test = object : BaseJUnitTest(config) {
+            val animal by dependency<Animal>()
+
+            override fun configure() = super.configure()
+                .requireReal<Animal>()
+        }
+
+        assert(test.animal::class == Cat::class)
+    }
+
+    @Test(expected = Exception::class)
+    fun `Locally configuring subtype doesn't configure supertype`() {
+
+        val config = moduleTestingConfiguration {
+            dependency any of<Animal>()
+        }
+
+        object : BaseJUnitTest(config) {
+            override fun configure() = super.configure()
+                .requireReal<Cat>() // already fails here
+        }
+    }
+
+}
