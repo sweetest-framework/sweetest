6856e64ea8bbc494890b91de55b0307479703b53
Marc Neureiter <mneureiter@juvavis.com>
2020-11-30 13:12:35 +0100

    Public interface to CoroutineDispatcher

--- sweetest/sweetest-api/src/main/java/com/mysugr/sweetest/framework/coroutine/CoroutinesTestContext.kt
+++ sweetest/sweetest-api/src/main/java/com/mysugr/sweetest/framework/coroutine/CoroutinesTestContext.kt
@@ -1,30 +1,26 @@
 package com.mysugr.sweetest.framework.coroutine
 
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.cancelAndJoin
-import java.util.concurrent.Executors
 import kotlin.coroutines.CoroutineContext
 
 /**
  * Experimental
  */
 internal class CoroutinesTestContext {
+
     private val name = CoroutineName("testCoroutine${instanceCounter++}")
     private val supervisorJob = SupervisorJob()
+
     val coroutineContext: CoroutineContext
-        get() = coroutineDispatcher + supervisorJob + name
+        get() = SweetestCoroutineSupport.coroutineDispatcher + supervisorJob + name
 
     suspend fun testFinished() {
         supervisorJob.cancelAndJoin()
     }
 
     companion object {
-        val coroutineDispatcher: CoroutineDispatcher by lazy {
-            Executors.newSingleThreadExecutor().asCoroutineDispatcher()
-        }
         private var instanceCounter = 0
     }
 }
