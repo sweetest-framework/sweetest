b619d584fe4c376a8ea4035f1b42b2a4846db5f4
Marc Neureiter <mneureiter@juvavis.com>
2020-11-30 13:44:44 +0100

    Deprecate utils

--- sweetest/sweetest-api/src/main/java/com/mysugr/sweetest/util/TestUtils.kt
+++ sweetest/sweetest-api/src/main/java/com/mysugr/sweetest/util/TestUtils.kt
@@ -4,12 +4,22 @@ import org.junit.Assert.fail
 import org.mockito.Mockito
 import org.mockito.internal.util.MockUtil
 
+private const val OUT_OF_SCOPE_DEPRECATION_MESSAGE =
+    "Phased out after v1 as out-of-scope."
+
+private const val MOCKITO_DEPRECATION_MESSAGE =
+    "Phased out after v1 as sweetest becomes independent of specific mocking frameworks."
+
+@Deprecated(MOCKITO_DEPRECATION_MESSAGE)
 inline fun <reified T> mock(): T = Mockito.mock(T::class.java)
 
+@Deprecated(MOCKITO_DEPRECATION_MESSAGE)
 val Any.isMock get() = MockUtil.isMock(this)
 
+@Deprecated(MOCKITO_DEPRECATION_MESSAGE)
 val Any.isSpy get() = MockUtil.isSpy(this)
 
+@Deprecated(OUT_OF_SCOPE_DEPRECATION_MESSAGE)
 inline fun <reified TException : Exception> expectException(block: () -> Unit) {
     try {
         block()
