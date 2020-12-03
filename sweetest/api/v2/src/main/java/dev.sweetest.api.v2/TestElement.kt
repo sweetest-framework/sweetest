23441a62154ec4358b8407677cd8dc2cc9dfd9f3
Marc Neureiter <mneureiter@juvavis.com>
2020-12-03 10:35:57 +0100

    Support suspend funs for Steps.invoke operator

--- sweetest/api/v2/src/main/java/dev.sweetest.api.v2/TestElement.kt
+++ sweetest/api/v2/src/main/java/dev.sweetest.api.v2/TestElement.kt
@@ -91,7 +91,7 @@ abstract class TestElement : com.mysugr.sweetest.internal.TestElement {
     inline fun <reified T : Steps> steps(): ReadOnlyProperty<TestElement, T> =
         stepsInternal(this, T::class)
 
-    operator fun <T : Steps> T.invoke(run: T.() -> Unit) = run(this)
+    inline operator fun <T : Steps> T.invoke(run: T.() -> Unit) = run(this)
 
     // --- region: Internals
 
